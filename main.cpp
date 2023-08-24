b409e54e3b2dac44252c7b81c67d6686bf9561e7
SharminNusrat <bsse1441@iit.du.ac.bd>
2023-08-24 11:31:55 +0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include "displayMap1.cpp"
 #include "dijkstra1.cpp"
 #include "dfsFindAllPath.cpp"
+#include "locationSearch.cpp"
+//#include "primsOptimalCost.cpp"
+#include "primsOptimalCost2.cpp"
 #include <bits/stdc++.h>
 #include <graphics.h>
 #define MAX_VERTICES 100
@@ -16,7 +19,7 @@ using namespace std;
 
 int main (void) {
 
-    int numVertices = 14;
+    int numVertices = 14, choice = 1;
 
 
     int gd = DETECT, gm;
@@ -28,15 +31,58 @@ int main (void) {
         };
 
 
-
     int rel_matrix[MAX_VERTICES][MAX_VERTICES] = {0};
 
 
+
+    displayGraph(rel_matrix, pos, numVertices);
     //setcolor(WHITE);
     //setfillstyle(SOLID_FILL, GREEN);
-    displayGraph(rel_matrix, pos, numVertices);
-    //userInputDijkstra(pos);
-    userInputDFS(pos);
+
+
+    //while(1) {
+
+        printf("----------------Menu----------------\n");
+        printf("1.Find Shortest Path\n");
+        printf("2.Search Location\n");
+        printf("3.Find All Paths\n");
+        printf("4.Find Optimal Cost\n");
+        printf("5.Exit\n");
+
+        printf("Enter your choice: ");
+        scanf("%d",&choice);
+        getchar();
+
+
+        switch(choice) {
+            case 1:
+                userInputDijkstra(pos);
+                break;
+            case 2:
+                userInputSearching(pos);
+                break;
+            case 3:
+                userInputDFS(pos);
+                break;
+            case 4:
+                userInputPrims(pos);
+                break;
+            case 5:
+                printf("Exiting the program.\n");
+                return 0;
+            default:
+                printf("Invalid choice. Please enter a valid choice.\n");
+        }
+
+
+
+    //}
+
+
+
+
+
+    //delay(5000);
 
     getch();
     closegraph();
